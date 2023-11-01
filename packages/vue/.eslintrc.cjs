a525bda7e474abe75a2b8f7f8daf1c8c7a7d12ed
Blro <gyeongtae020319@gmail.com>
2023-11-02 00:42:16 +0900

    chore: Setup ESLint for vue

---
+++ packages/vue/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict",
+    "plugin:vue/vue3-strongly-recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+};
